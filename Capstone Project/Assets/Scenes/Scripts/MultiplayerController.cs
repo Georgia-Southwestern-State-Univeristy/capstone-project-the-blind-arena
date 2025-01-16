19f9ae6c1d62d11bce9f8ebf0a8115c432b49288
Sykure <davidh0618@gmail.com>
2025-01-16 15:01:11 -0500

    Multiplayer Prep

--- Capstone Project/Assets/Scenes/Scripts/MultiplayerController.cs
+++ Capstone Project/Assets/Scenes/Scripts/MultiplayerController.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class MultiplayerController : MonoBehaviour
 {
     public float playerSpeed;
     public Rigidbody rb;
     public SpriteRenderer sr;
-    
+    private Alteruna.Avatar _avatar;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _avatar = GetComponent<Alteruna.Avatar>();
+
+        if (!_avatar.IsMe)
+            return;
+
         rb = GetComponent<Rigidbody>();
 
     }
@@ -16,12 +22,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!_avatar.IsMe)
+            return;
+
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        Vector3 moveDir = new Vector3(x,0,y*2);
+        Vector3 moveDir = new Vector3(x, 0, y * 2);
 
         //Movement code
-        rb.linearVelocity = moveDir*playerSpeed;
+        rb.linearVelocity = moveDir * playerSpeed;
 
         //Flip if sprite moves in an opposite direction
         if (x != 0 && x < 0)
