a3ae550ec046899169eae3882cc492355cf65660
Voxaire <82889199+Voxaire@users.noreply.github.com>
2025-01-16 15:59:22 -0500

    Gravity Control

--- Capstone Project/Assets/Scenes/Scripts/Character/PlayerController.cs
+++ Capstone Project/Assets/Scenes/Scripts/Character/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 3f;
+    [SerializeField] private float gravityScale = 1f; // New variable to control gravity
 
     private Rigidbody rb;
     private Vector3 moveDir;
@@ -17,6 +18,7 @@ public class PlayerController : MonoBehaviour
         // Ensure Rigidbody settings are correct
         rb.interpolation = RigidbodyInterpolation.Interpolate;
         rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        rb.useGravity = false; // Disable default gravity
     }
 
     void Update()
@@ -28,21 +30,28 @@ public class PlayerController : MonoBehaviour
 
         // Movement input
         moveDir.x = Input.GetAxis("Horizontal");
-        moveDir.z = Input.GetAxis("Vertical");
+        moveDir.z = 2 * Input.GetAxis("Vertical");
     }
 
     private void FixedUpdate()
     {
         Move();
+        ApplyCustomGravity();
     }
 
     private void Move()
     {
-        // Maintain Y velocity (gravity) and apply movement in XZ plane
-        Vector3 velocity = new Vector3(moveDir.x, rb.linearVelocity.y, moveDir.z) * speed;
-        rb.linearVelocity = velocity; // Use rb.velocity for Rigidbody movement
+        // Maintain Y velocity and apply movement in the XZ plane
+        Vector3 velocity = new Vector3(moveDir.x * speed, rb.linearVelocity.y, moveDir.z * speed);
+        rb.linearVelocity = velocity;
 
         CheckForFlipping();
+    }
+
+    private void ApplyCustomGravity()
+    {
+        // Apply custom gravity scaling
+        rb.linearVelocity += new Vector3(0, Physics.gravity.y * gravityScale * Time.fixedDeltaTime, 0);
     }
 
     private void CheckForFlipping()
